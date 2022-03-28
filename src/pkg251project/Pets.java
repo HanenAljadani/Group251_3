ac3e73e8494a076053cd820aa4305648f2f2845a
mohan <mohan@10.24.35.130>
2022-03-28 13:25:25 +0300

    Add Pet Class

--- src/pkg251project/Pets.java
+++ src/pkg251project/Pets.java
@@ -9,6 +9,36 @@ package pkg251project;
  *
  * @author 96653
  */
+//jamela hadi
 public class Pets {
-    
+
+    private String ill;
+    private String bloodtype;
+
+    String type;
+
+    public String getillness() {
+        return ill;
+
+    }
+
+    public void setillness(String illness) {
+        this.ill = illness;
+    }
+
+    public String getbloodtype() {
+        return bloodtype;
+    }
+
+    public void setbloodgroup(String bloodtype) {
+        this.bloodtype = bloodtype;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
