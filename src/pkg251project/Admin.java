96d25da8c5f3863b2cbfd2b199c2a8fb470e0afb
hp <hp@192.168.100.3>
2022-03-31 18:20:29 +0300

    bashayer  Admin class

--- src/pkg251project/Admin.java
+++ src/pkg251project/Admin.java
@@ -10,5 +10,31 @@ package pkg251project;
  * @author 96653
  */
 public class Admin {
+    int id;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Admin(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Admin() {
+    }
     
 }
